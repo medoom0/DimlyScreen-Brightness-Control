c576170b6fb3de775d97e33daef821df7b7f43ff
medoom0 <medooali91@gmail.com>
2025-02-23 21:03:20 +0200

    Add project files.

---
+++ Dimly/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Dimly
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.resetButton = new System.Windows.Forms.Button();
+            this.brightnessSlider = new System.Windows.Forms.TrackBar();
+            this.brightnessLabel = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.brightnessSlider)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(118, 125);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(157, 29);
+            this.resetButton.TabIndex = 0;
+            this.resetButton.Text = "Reset to Default";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // brightnessSlider
+            // 
+            this.brightnessSlider.Location = new System.Drawing.Point(83, 63);
+            this.brightnessSlider.Maximum = 100;
+            this.brightnessSlider.Name = "brightnessSlider";
+            this.brightnessSlider.Size = new System.Drawing.Size(228, 56);
+            this.brightnessSlider.TabIndex = 1;
+            this.brightnessSlider.TickFrequency = 10;
+            this.brightnessSlider.Value = 30;
+            this.brightnessSlider.Scroll += new System.EventHandler(this.brightnessSlider_Scroll);
+            // 
+            // brightnessLabel
+            // 
+            this.brightnessLabel.AutoSize = true;
+            this.brightnessLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.brightnessLabel.Location = new System.Drawing.Point(113, 26);
+            this.brightnessLabel.Name = "brightnessLabel";
+            this.brightnessLabel.Size = new System.Drawing.Size(155, 25);
+            this.brightnessLabel.TabIndex = 2;
+            this.brightnessLabel.Text = "Brightness: 30%";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Exit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(103, 28);
+            // 
+            // Exit
+            // 
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(102, 24);
+            this.Exit.Text = "Exit";
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Dimly - Brightness Control";
+            this.notifyIcon.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 166);
+            this.Controls.Add(this.brightnessLabel);
+            this.Controls.Add(this.brightnessSlider);
+            this.Controls.Add(this.resetButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dimly - Brightness Controller";
+            ((System.ComponentModel.ISupportInitialize)(this.brightnessSlider)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.TrackBar brightnessSlider;
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.Label brightnessLabel;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Exit;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ Dimly/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Dimly
+{
+    public partial class Form1 : Form
+    {
+        private Form overlay;
+        private Timer clickThroughTimer;
+        private ContextMenuStrip trayMenu;
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        private const int GWL_EXSTYLE = -20;
+        private const int WS_EX_LAYERED = 0x80000;
+        private const int WS_EX_TRANSPARENT = 0x20;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CreateOverlay();
+            InitializeTimer();
+            SetupTrayMenu();
+        }
+
+        private void CreateOverlay()
+        {
+            overlay = new Form
+            {
+                FormBorderStyle = FormBorderStyle.None,
+                BackColor = Color.Black,
+                Opacity = 0.3,
+                ShowInTaskbar = false,
+                TopMost = true,
+                WindowState = FormWindowState.Maximized
+            };
+            overlay.Show();
+            MakeOverlayClickThrough();
+        }
+
+        private void InitializeTimer()
+        {
+            clickThroughTimer = new Timer();
+            clickThroughTimer.Interval = 300; 
+            clickThroughTimer.Tick += (sender, e) =>
+            {
+                MakeOverlayClickThrough();
+                clickThroughTimer.Stop();
+            };
+        }
+
+        private void SetupTrayMenu()
+        {
+            trayMenu = new ContextMenuStrip();
+            trayMenu.Items.Add("Exit", null, exitToolStripMenuItem_Click);
+            notifyIcon.ContextMenuStrip = trayMenu;
+        }
+
+        private void MakeOverlayClickThrough()
+        {
+            int style = GetWindowLong(overlay.Handle, GWL_EXSTYLE);
+            SetWindowLong(overlay.Handle, GWL_EXSTYLE, style | WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        }
+
+        private void brightnessSlider_Scroll(object sender, EventArgs e)
+        {
+            overlay.Opacity = brightnessSlider.Value / 100.0;
+            brightnessLabel.Text = $"Brightness: {brightnessSlider.Value}%";
+
+            clickThroughTimer.Stop(); 
+            clickThroughTimer.Start(); 
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            brightnessSlider.Value = 30;
+            brightnessLabel.Text = "Brightness: 30%";
+
+            overlay.Opacity = 0.3;
+            MakeOverlayClickThrough(); 
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
